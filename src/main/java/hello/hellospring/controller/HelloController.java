9f4f0f8a4a9d09c73d62ab5a9db3121ecaf8d7a8
Say-young <seyoung990525@gmail.com>
2020-07-27 23:42:04 +0900

    정적컨텐츠/MVC

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,7 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -17,4 +18,11 @@ public class HelloController {
     }
 
     //spring-boot-devtools 라이브러리 찾아보기
+
+    @GetMapping("hello-mvc")
+    //http://localhost:8080/hello-mvc?name=어쩌구
+    public String helloMvc(@RequestParam("name") String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
 }
